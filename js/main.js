a25ea1481b31f7c4e49abb97cb6d9df2a428d6cd
Viktor <Viktor.Popov@dataart.com>
2021-03-18 13:45:44 +0200

    implement counting

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// categorization and filtering functionality
 const category = document.querySelector(".category")
 const maxPrice = document.querySelector(".max-price")
 const items = document.querySelectorAll(".product-box__item")
@@ -27,4 +28,37 @@ function filter(category, price) {
       item.classList.add("hidden")
     }
   })
+}
+
+// counting functionality
+
+const box = document.querySelector(".products-box")
+const amountText = document.querySelector("#amount")
+const dishesNumberText = document.querySelector("#items-number")
+
+amountText.textContent = 0
+dishesNumberText.textContent = 0
+
+box.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("product-box__btn")) {
+    return
+  }
+  amountText.textContent = countTotalAmount()
+  dishesNumberText.textContent = countTotalDishesNumber()
+})
+
+function countTotalDishesNumber() {
+  let val = 0
+  items.forEach(item => {
+    val += +item.querySelector(".qty__item").value
+  })
+  return val
+}
+
+function countTotalAmount() {
+  let amount = 0
+  items.forEach(item => {
+    amount += +item.querySelector(".qty__item").value * +item.dataset.price
+  })
+  return amount
 }
